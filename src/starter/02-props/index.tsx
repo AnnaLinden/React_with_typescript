8765505d660d8f17896a13335ab932a6146c17e1
AnnaLinden <linden.a.anna@gmail.com>
2024-07-01 20:41:47 +0300

    react typescript props

--- src/starter/02-props/index.tsx
+++ src/starter/02-props/index.tsx
@@ -1,8 +1,11 @@
-function Component() {
+type ComponentProps = {name: string, id: number, children?: React.ReactNode };
+
+function Component({name, id, children}: ComponentProps) {
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Props</h2>
+      <h1>Name: {name}</h1>
+      <h2>Id: {id}</h2>
+      {children}
     </div>
   );
 }
