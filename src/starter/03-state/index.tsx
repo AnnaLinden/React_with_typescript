00ffa80fb4b8e4d050e4c5972a67125c35ac4ede
AnnaLinden <linden.a.anna@gmail.com>
2024-07-03 21:54:13 +0300

    use state with object

--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,15 +1,41 @@
 import { useState } from "react";
 
+type Link = {
+  id: number;
+  url: string;
+  text: string;
+};
+
+const navLinks: Link[] =[
+  {
+    id: 1,
+    url: 'https://reactjs.org',
+    text: 'react docs',
+  },
+  {
+    id: 2,
+    url: 'https://reactrouter.com',
+    text: 'react router docs',
+  },
+  {
+    id: 3,
+    url: 'https://reacttraining.com',
+    text: 'react training',
+  },
+]
+
 function Component() {
   const [text, setText] = useState('some text');
   const [number, setNumber] = useState(0);
   const [list, setList] = useState<number[]>([]);
+  const [links, setLinks] = useState<Link[]>(navLinks);
 
   const handleClick = ()=> {
 
     setText(currentText => currentText === "some text" ? "text changed" : "some text")
     setNumber(prevNumber => prevNumber+1);
     setList(prevList => [...prevList, number+1]);
+    setLinks([...links, { id: 4, url: 'hello', text: 'hello' }]);
 
   }
   return (
@@ -24,6 +50,13 @@ function Component() {
         {list.map((item, index)=> (
           <li key={index}> {item}</li>
         ))}
+      </ul>
+      <ul>
+        {links.map((link)=>
+        <li key = {link.id}>
+          <a href="{link.url}">{link.text}</a>
+        </li>
+        )}
       </ul>
      
     </div>
